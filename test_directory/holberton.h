3ee61f5c7e857e5d3e791b6c49abb3445fc5814c
klitscher <672@holbertonschool.com>
2019-03-17 21:22:56 +0000

    Add header file and _strlen helper function

---
+++ test_directory/holberton.h
@@ -0,0 +1,21 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*helper functions*/
+int _strlen(char *s);
+
+/**
+ * struct data_types - the types of data we can have
+ * @c: char data type
+ * @s: string data type
+ */
+typedef struct data_types
+{
+	char c;
+	void (*f) (va_list);
+}types;
+
+/* core functions */
+int _printf(const char *format, ...);
+
+#endif /* _HOLBERTON_H_ */
